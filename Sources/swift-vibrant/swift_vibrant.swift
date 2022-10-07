36dad06c2d43e9448f76f28e26d7d4ac6b750a55
Jacob Zyla <jzyla@branchsm.com>
2022-10-07 00:42:23 -0400

    inital

--- Sources/swift-vibrant/swift_vibrant.swift
+++
@@ -1,6 +0,0 @@
-public struct swift_vibrant {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
--- Tests/swift-vibrantTests/swift_vibrantTests.swift
+++ Tests/swift-vibrantTests/swift_vibrantTests.swift
@@ -6,6 +6,5 @@ final class swift_vibrantTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(swift_vibrant().text, "Hello, World!")
     }
 }
